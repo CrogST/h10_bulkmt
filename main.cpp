410e33bb663f75bf08d4e502423a6bb760f90af4
igor <samanovis@energomera.ru>
2019-12-05 01:03:32 +0300

    * убран отладочный код

--- main.cpp
+++ main.cpp
@@ -4,8 +4,6 @@
 #include "collect.h"
 #include "class_out.h"
 
-#include <vector>
-
 int main(int argc, char *argv[])
 {
     if(argc <= 1) return -1;
@@ -16,39 +14,6 @@ int main(int argc, char *argv[])
     write_out fl(&rp);
 
     collect col{n};
-
-    {
-
-        std::vector<std::string> vct = {
-            "{",
-            "00",
-            "01",
-            "02",
-            "03",
-            "04",
-            "}",
-            "000",
-            "001",
-            "002",
-            "003",
-            "{",
-            "0000",
-            "0001",
-            "0002",
-            "0003",
-            "0004",
-            "}"
-        };
-
-        for(const auto & str : vct) {
-            auto res = col.handle(str);
-            if(res) {
-                auto val = res.value();
-                rp.notify_all(std::get<0>(val), std::get<1>(val));
-            }
-        }
-    }
-
     for(std::string line; std::getline(std::cin, line); )
     {
         auto res = col.handle(line);
