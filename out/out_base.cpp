e4b701cc6d587874e48915513972b40a4e317bf9
igor <samanovis@energomera.ru>
2020-03-28 14:06:44 +0300

    * более элегантный lock

--- out/out_base.cpp
+++ out/out_base.cpp
@@ -34,13 +34,9 @@ std::tuple<int, int> out_base::thread_exec() {
         sleep_mx.unlock();
 
         cv.wait(lk, [&] {
-            sleep_mx.lock();
-            q_mx.lock();
-            auto sl = sleep;
-            auto q_tmp = q;
-            sleep_mx.unlock();
-            q_mx.unlock();
-            return !sl || q_tmp;
+            std::lock_guard(sleep_mx);
+            std::lock_guard(q_mx);
+            return !sleep || q;
         });
         _log("thread " << std::this_thread::get_id() << ": " << "wake up" << std::endl)
 
